fbda74b3078d3b23e5bb5f5b2d60580b02610a59
Kamaljit Bhullar Desktop <kamaljitbhuller@gmail.com>
2021-03-08 12:31:38 -0800

    created html, css and javascript files. they all work in correct manner

---
+++ app.js
@@ -0,0 +1,28 @@
+
+let variable1 = 25
+
+let variable2 = 50
+
+const myName = 'Kamaljit'
+
+const boolean = true
+
+if (variable1 === variable2){
+  console.log('they match')
+}else {
+  console.log('no luck')
+}
+
+variable2 = 25
+
+if (variable1 === variable2){
+  console.log('they match')
+}else {
+  console.log('no luck')
+}
+
+for (chars of myName){
+  console.log(chars)
+}
+
+
